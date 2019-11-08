693d3341725d0fec2e4df724b398bbacf66ff152
jzonthemtn <jeff.zemerick@mtnfog.com>
2019-11-08 13:16:55 -0500

    Fixing filename.

--- copy-to-distribution.sh
+++ copy-to-distribution.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 rm -f ./distribution/*.jar
-cp ./philter-profile-registry-app/target/phiilter-profile-registry.jar ./distribution/
+cp ./philter-profile-registry-app/target/philter-profile-registry.jar ./distribution/
 cp ./target/generated-sources/license/THIRD-PARTY.txt ./distribution/NOTICE.txt
 
